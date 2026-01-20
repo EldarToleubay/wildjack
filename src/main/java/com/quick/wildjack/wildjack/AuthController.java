0de454a1053d0ed98f3e6cac662bd3d2832fd5ba
EldarToleubay <49515746+EldarToleubay@users.noreply.github.com>
2026-01-20 15:12:50 +0500

    sad

--- src/main/java/com/quick/wildjack/wildjack/AuthController.java
+++ src/main/java/com/quick/wildjack/wildjack/AuthController.java
@@ -1,6 +1,7 @@
 package com.quick.wildjack.wildjack;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @CrossOrigin("*")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final TelegramAuthService telegramAuthService;
 
     @PostMapping("/telegram-lite")
     public ResponseEntity<TelegramAuthResponse> telegramLite(@RequestBody TelegramAuthRequest request) {
+        log.info("Telegram lite request received");
+        log.info("InitData:"+ request.getInitData());
+        log.info("user.id:"+ request.getUser().getId());
+        log.info("user.photourl:"+ request.getUser().getPhotoUrl());
         return ResponseEntity.ok(telegramAuthService.authenticate(request));
     }
 }
